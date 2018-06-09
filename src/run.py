93bbc5b26aa3ba24303cdb60ece91a1ae6fc6ee5
hourann <tdkgn@outlook.com>
2018-06-09 15:32:14 +0800

    [Hourann] Beta release.

--- src/run.py
+++ src/run.py
@@ -5,26 +5,25 @@ from src.table import Table
 ALIVE_CHAR = '*'
 DEAD_CHAR = ' '
 STATE1 = [[False, True, False], [False, True, False], [False, True, False]]
-table = Table(3, 3)
-table.set_state(STATE1)
-
-def answer():
-    return 42
+table = Table(10, 80)
+table.generate_random_state()
+# table.set_state(STATE1)
 
 
-def print_state(window, state):
-    height = len(state)
-    width = len(state[0])
+def print_table(window, table):
+    width, height = table.get_size()
+    state = table.get_state()
     for x in range(width):
         for y in range(height):
             window.addstr(x, y, ALIVE_CHAR if state[y][x] else DEAD_CHAR)
             window.refresh()
 
-def game_of_life(window):
-    state = table.get_state()
-    print_state(window, state)
-    table.next()
-    time.sleep(0.5)
-    game_of_life(window)
+def draw(window):
+    while True:
+        print_table(window, table)
+        table.next()
+        time.sleep(1)
 
-curses.wrapper(game_of_life)
+
+if __name__ == '__main__':
+    curses.wrapper(draw)
--- src/table.py
+++ src/table.py
@@ -1,11 +1,10 @@
 from src.cell import Cell
-
+from random import random
 
 class Table:
-    def __init__(self, width, height, interval=500, init_state=None):
+    def __init__(self, width, height, init_state=None):
         self._width = width
         self._height = height
-        self._interval = interval
         self._cells = [[Cell(True) for x in range(width)] for y in range(height)]
         self._time = 0
 
@@ -30,6 +29,9 @@ class Table:
     def _get_cell(self, x, y):
         return self._cells[y][x]
 
+    def get_size(self):
+        return self._width, self._height
+
     def next(self):
         self._time += 1
         num_of_alive_neighbor_table = [[0] * self._width for _ in range(self._height)]
@@ -39,3 +41,8 @@ class Table:
         for x in range(self._width):
             for y in range(self._height):
                 self._get_cell(x, y).next(num_of_alive_neighbor_table[y][x])
+
+    def generate_random_state(self, ratio_of_alive=.5):
+        for row in self._cells:
+            for cell in row:
+                cell.state = True if random() < ratio_of_alive else False
