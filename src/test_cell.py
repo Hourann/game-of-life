948d6be00e99a073c51a715130e3229995659f6c
hourann <tdkgn@outlook.com>
2018-06-09 11:02:27 +0800

    [Wang Haimin]Cell will turn dead if num of neighbor > 3

--- src/test_cell.py
+++ src/test_cell.py
@@ -4,13 +4,25 @@ from src.Cell import Cell
 
 
 class TestCell(TestCase):
-    def test_should_turn_dead_when_a_live_cell_have_less_than_2_alive_neigbhour(self):
+    def test_should_turn_dead_when_a_live_cell_have_2_alive_neighbour(self):
         # given
         cell = Cell(True)
-        number_of_alive_neigbour = 1
+        number_of_alive_neighbour = 1
 
-        # whenR
-        cell.next(number_of_alive_neigbour)
+        # when
+        cell.next(number_of_alive_neighbour)
 
         # then
         self.assertFalse(cell.state)
+
+    def test_should_turn_dead_when_a_cell_have_3_alive_neighbour(self):
+        # given
+        cell = Cell(True)
+        number_of_alive_neighbour = 4
+
+        # when
+        cell.next(number_of_alive_neighbour)
+
+        # then
+        self.assertFalse(cell.state)
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,7 @@ import unittest
 from src.run import answer
 class TestMain(unittest.TestCase):
     def test_hello(self):
-        self.assertEqual(answer(), 40)
+        self.assertEqual(answer(), 42)
 
 
 if __name__ == '__main__':
